5f6894372ba2d6d507a4128a9ad5a839c1a92b03
ELOUAIZI AYOUB <ayoubelouaizi@gmail.com>
2023-01-18 02:03:48 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+
+const api = {
+  key: '380ac89aac383e334642ff7f05ec0fe0',
+  base: 'https://api.openweathermap.org/data/2.5/'
+}
+
+function App() {
+  return (
+    <div className="App">
+      <header className='App-header'>
+      <h1>Wheader App</h1>
+      </header>
+
+    </div>
+  );
+}
+export default App;
